f6cda8d1741a9ae50c2e2ac1d22c5785d923a7df
HideyukiWanaka <113268470+HideyukiWanaka@users.noreply.github.com>
2025-04-07 17:50:18 +0900

    Update script.js

--- gait_analysis_app/static/Js/script.js
+++ gait_analysis_app/static/Js/script.js
@@ -1,1 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cameraView = document.getElementById('cameraView');
 
+    function startCamera() {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({
+                video: { facingMode: { exact: 'environment' } } // 環境カメラを指定
+            })
+            .then(function(stream) {
+                cameraView.srcObject = stream;
+            })
+            .catch(function(error) {
+                console.error('カメラへのアクセスに失敗しました:', error);
+            });
+        } else {
+            console.error('getUserMedia APIに対応していません。');
+        }
+    }
+
+    startCamera();
+});
